e3816703cadd1c4aad2c579669449fb2370190f0
Nafa06 <naufalanggardi09@gmail.com>
2025-05-22 10:17:29 +0700

    melengkapkan fungsi pada bagian if else

--- file1.cpp
+++ file1.cpp
@@ -60,6 +60,14 @@ public:
         else
         {
             Node *current = top;
-        }
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // return the value of the top node
     }
+
+    // IsEmpty operation: check if the stack is empty
 };
