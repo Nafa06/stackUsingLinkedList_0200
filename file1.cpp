8090e1b0926d41ea46ac7773199b8fa8fc981d03
Nafa06 <naufalanggardi09@gmail.com>
2025-05-22 10:06:39 +0700

    Menambahkan fungsi pop

--- file1.cpp
+++ file1.cpp
@@ -17,20 +17,32 @@ public:
 // stack class
 class Stack
 {
-    private:
-    Node* top; // pointer to the top node of the stack
+private:
+    Node *top; // pointer to the top node of the stack
 
-    public:
-    Stack(){
+public:
+    Stack()
+    {
         top = NULL; // initialize the stack with a null top pointer
     }
 
     // push operation: insert an element onto the top of the stack
-    int push(int value){
-        Node* newNode = new Node(); // 1. allocate memory for the new node
-        newNode->data = value; // 2. assign value
-        newNode->next = top; // 3. set the next pointer of the new node to the current top
-        top = newNode; // 4. update the top pointer to the new node
+    int push(int value)
+    {
+        Node *newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value;      // 2. assign value
+        newNode->next = top;        // 3. set the next pointer of the new node to the current top
+        top = newNode;              // 4. update the top pointer to the new node
         cout << "Push Value: " << value << endl;
+        return value;
+    }
+
+    // pop operation: remove the topmost element from the stack
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty," << endl;
+        }
     }
 };
