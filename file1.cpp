2cc00b6055be7c1364012eb409e98e981cb43ea5
Nafa06 <naufalanggardi09@gmail.com>
2025-05-22 09:54:01 +0700

    Menambahkan fungsi push

--- file1.cpp
+++ file1.cpp
@@ -24,4 +24,13 @@ class Stack
     Stack(){
         top = NULL; // initialize the stack with a null top pointer
     }
+
+    // push operation: insert an element onto the top of the stack
+    int push(int value){
+        Node* newNode = new Node(); // 1. allocate memory for the new node
+        newNode->data = value; // 2. assign value
+        newNode->next = top; // 3. set the next pointer of the new node to the current top
+        top = newNode; // 4. update the top pointer to the new node
+        cout << "Push Value: " << value << endl;
+    }
 };
