16c0f9fb29bd559a042a6be1194f1d183a0a94de
Nafa06 <naufalanggardi09@gmail.com>
2025-05-22 10:39:55 +0700

    memperbaiki penempatan case 4 exit

--- file1.cpp
+++ file1.cpp
@@ -119,9 +119,9 @@ int main()
                 cout << "Stack is empty. No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         }
-    case 4:
-        cout << "Exiting program." << endl;
-        break;
     }
 }
